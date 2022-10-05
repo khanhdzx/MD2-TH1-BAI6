bdd6b0179ebf0d58b4a41479867be6818a45cbd4
khanhdzx <khanhvannguyen2303@gmail.com>
2022-10-05 15:10:08 +0700

    ja

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+
+        triangle = new Triangle(3,3,3);
+        System.out.println(triangle);
+    }
+}
